ac77fae25cf85e53e2e84d11be365285ae1ec359
seojeonghoo <seojh0800@gmail.com>
2026-08-20 12:33:06 +0900

    뷁

---
+++ 01_datatype/ex1.py
@@ -0,0 +1,35 @@
+# # 변수 
+# a = 2
+# b = 3
+
+# print(a, end='')
+# print(b)
+# print(a, b)
+# print(a, b, sep=',')
+
+# a, b = 2, 3
+# print(a, b) # 튜플 언패킹
+
+
+# a = b = c = 0
+
+
+# 값 swap
+a, b = 2, 3
+
+temp = a
+a = b
+b = temp
+
+
+# 변수명 규칙 (C와 동일)
+# 알파벳, 숫자, 언더스코어(_)로 구성
+# 숫자로 시작할 수 없음
+# 대소문자 구분
+# 예약어 사용 불가
+
+# snake_case : 소문자 + 언더스코어
+# camelCase : 소문자 + 대문자
+
+이름 = '홍길동'
+print(이름)
