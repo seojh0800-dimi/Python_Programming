6bd5ec3425bfc1e925e789b45c66e3926902826d
seojeonghoo <seojh0800@gmail.com>
2026-08-21 10:36:44 +0900

    Good bye

--- 01_datatype/ex1.py
+++ 01_datatype/ex1.py
@@ -1,29 +1,19 @@
-# 변수
-a = 2
-b = 3
-print(a,end="")
-print(b)
-print(a, b, sep="")
+#불리언
+# True of False
+a=True
+print(a,type(a))
 
-a,b = 2,3
+print(1<0)
+print(1>0)
+print(1==0)
 
-print (a,b)
+print("apple" > "apble")
 
-a = b = c = 0
+print(bool(0))
+print(bool(""))
+print(bool([]))
 
-# 값 swap
-a,b = 2,3
-a,b = b,a
-
-print(a,b)
-
-# 변수명 규칙 (C와 동일)
-# 알파벳, 숫자, 특수문자 (_)만 가능
-# 숫자로 시작 불가
-# 예약어 사용 불가
-# snake_case
-# camelCase
-# strHungarianCase
-# !name = "pororo"
-# 2name = "pororo"
-#class = "test"
+#None 자료형
+a = None
+print(a,type(a))
+print(bool(a))
--- 01_datatype/ex2.py
+++ 01_datatype/ex2.py
@@ -1,48 +1,48 @@
-#파이썬 자료형
-#1. 기본 자료형: 숫자형 (정수형, 실수형), 불리언, 문자열
-#2. 컬렉션 자료형: 리스트, 튜플, 딕셔너리, 집합
+# 자료형
+# 1. 기본 자료형: 숫자형(정수형, 실수형), 불리언, 문자열
+# 2. 컬렉션 자료형: 리스트, 튜플, 딕셔너리, 집합
 
-# 숫자형 (정수형)
-#C언어: char short int long long long int
-# int
+# 숫자형(정수형)
+# C언어 : char / short / int / long / long long
+# 파이썬 : int
 
 a = 10
 print(a, type(a))
 
-#2진수, 8진수, 16진수
-print(bin(a),oct(a),hex(a));
-print(ord("A"),chr(65))
+# 2진수, 8진수, 16진수
+print(bin(a), oct(a), hex(a))
+print(ord("A"), chr(65))
 
-#정수형의 데이터 표현 범위
-x = pow(10,pow(10,2))
+# 정수형의 데이터 표현 범위
+# int 자료형의 표현 범위에는 제한이 없음
+x = 10 ** 1000
 print(x)
 
-#int 자료형의 표현 범위는 제한이 없음
-#오버플로우 테스트
-a = pow(2,31)-1
-a+=1
+# 오버플로우
+a = 2 ** 31 - 1
+a = a + 1
 print(a)
 
-# 실수형(float)
+# 실수형
+# C 언어: double / float
+# 파이썬: float
 b = 3.14
 print(b, type(b))
 
-#실수형의 표현 범위 제한
-# 부동소수점 방식: 64비트 = 부호 1비트 + 지수부 11비트 = 가수부 52비트
+# 실수형의 표현 범위
+# 부동소수점 방식: 64비트 = 부호(1비트) + 지수부(11비트) + 가수부(52비트)
 
 import sys
-print(sys.float_info.min)
-print(sys.float_info.max)
+print(sys.float_info.min, sys.float_info.max)
+print(-sys.float_info.min, -sys.float_info.max)
 
-
-a = 1.7*pow(10,308)
-b = 1.8*pow(10,308)
-
+a = 1.7e308
+b = 1.8e308
 print(a,b)
 
-#실수의 오차
-print(0.1+0.2)
-print(0.1+0.2 == 0.3)
+# 실수의 오차
+print(0.1 + 0.2)
+print(0.1 + 0.2 == 0.3)
 print(f"{0.1:.20f}")
 print(f"{0.2:.20f}")
 
@@ -50,4 +50,3 @@ print(f"{0.2:.20f}")
 print(float(10))
 print(int(3.14))
 print(int("100"))
-print(float("3.14"))
