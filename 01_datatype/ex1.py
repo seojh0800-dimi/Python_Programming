c4daaf3b3d448db37f524c1ad3056ce602b749ac
seojeonghoo <seojh0800@gmail.com>
2026-08-21 10:30:20 +0900

    뷁

--- 01_datatype/ex1.py
+++ 01_datatype/ex1.py
@@ -1,35 +1,29 @@
-# # 변수 
-# a = 2
-# b = 3
+# 변수
+a = 2
+b = 3
+print(a,end="")
+print(b)
+print(a, b, sep="")
 
-# print(a, end='')
-# print(b)
-# print(a, b)
-# print(a, b, sep=',')
+a,b = 2,3
 
-# a, b = 2, 3
-# print(a, b) # 튜플 언패킹
+print (a,b)
 
-
-# a = b = c = 0
-
+a = b = c = 0
 
 # 값 swap
-a, b = 2, 3
+a,b = 2,3
+a,b = b,a
 
-temp = a
-a = b
-b = temp
-
+print(a,b)
 
 # 변수명 규칙 (C와 동일)
-# 알파벳, 숫자, 언더스코어(_)로 구성
-# 숫자로 시작할 수 없음
-# 대소문자 구분
+# 알파벳, 숫자, 특수문자 (_)만 가능
+# 숫자로 시작 불가
 # 예약어 사용 불가
-
-# snake_case : 소문자 + 언더스코어
-# camelCase : 소문자 + 대문자
-
-이름 = '홍길동'
-print(이름)
+# snake_case
+# camelCase
+# strHungarianCase
+# !name = "pororo"
+# 2name = "pororo"
+#class = "test"
