5b0b7f0f7424239ce935b46bdfb1d3c30d05b994
seojeonghoo <seojh0800@gmail.com>
2026-08-25 14:06:56 +0900

    s

--- 01_datatype/ex4.py
+++ 01_datatype/ex4.py
@@ -2,3 +2,21 @@
 #"", ''
 a = "python"
 print(a, type(a))
+
+
+pi = 3.141592
+
+print(f"{pi:.3f}")
+print(f"{pi:.0f}")
+
+num = 132456789
+print(f"{num:,}")
+print(f"{num:15d}")
+print(f"{num:>15d}")
+print(f"{num:<15d}")
+print(f"{num:^15d}")
+print(f"{num:*>15d}")
+print(f"{num:*>+15d}")
+print(f"{num:*>+15,d}")
+print(f"{num:15,d}")
+
