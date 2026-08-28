80490faff5b15fe1aabcee534c0f35c6120f32c9
seojeonghoo <seojh0800@gmail.com>
2026-08-28 10:07:24 +0900

    sfsfd

---
+++ 02_operator/ex1.py
@@ -0,0 +1,15 @@
+# 연산자
+
+# 산술 연산자
+
+a = 10
+b = 3
+
+print(a + b)        # 덧셈
+print(a - b)        # 뺄셈
+print(a * b)        # 곱셈
+print(a / b)        # 나눗셈
+print(a // b)       # 몫
+print(a % b)        # 나머지
+print(a ** b)       # 제곱
+
