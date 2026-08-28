89919948401e028cfc7cef5825e0609f1a8f9a4a
seojeonghoo <seojh0800@gmail.com>
2026-08-28 10:42:32 +0900

    asdfasdf

--- 02_operator/ex1.py
+++ 02_operator/ex1.py
@@ -5,11 +5,54 @@
 a = 10
 b = 3
 
-print(a + b)        # 덧셈
-print(a - b)        # 뺄셈
-print(a * b)        # 곱셈
-print(a / b)        # 나눗셈
-print(a // b)       # 몫
-print(a % b)        # 나머지
-print(a ** b)       # 제곱
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# 복합 대입 연산자
+a= 0
+a += 4
+print(a)  # 4
+
+a -= 2
+print(a)  # 2
+
+# 증감 연산자 => 없음
+# b = a++
+# a += 1
+
+# 비교 연산자
+print(3 == 3)  # True
+print(3 == 3.0)  # True
+print(3 != 4)  # True
+print(3 > 2)  # True
+print(3 < 4)  # True
+print(3 >= 3)  # True
+print(3 <= 4)  # True
+print("apple" == "apple")  # True
+print("apple" == "apble")  # False
+print(1 < 2 < 3)  # True
+
+# 논리 연산자 (and , or , not)
+a = True
+b = False
+
+print(a and b)  # False
+print(a or b)  # True
+print(not b)  # True
+
+# Short-circuit 테스트
+a = 10
+b = 0
+
+#print(a/b)
+
+if a > 0 or a / b:
+    print("Yes")
+else:
+    print("No")
 
---
+++ 02_operator/ex2.py
@@ -0,0 +1,42 @@
+# 비트 연산자 
+a = 5                # 0000 0101
+b = 3                # 0000 0011
+print(a & b)         # 0000 0001
+print(a | b)         # 0000 0111
+print(a ^ b)         # 0000 0110
+print(a << 1)        # 5 -> 10 -> 20 -> 40
+print(40 >> b)       # 5
+print(~a)            # 1111 1010 -> 1111 1010 (-6)
+
+
+# 멤버십 연산자
+print("a" in "apple")  # True
+print(3 in [1, 2, 3])  # True
+
+# 삼항 연산자
+# int max = a > b ? a : b ( C언어 )
+# 파이썬에서는 아래와 같이 표현
+a = 10
+b = 20
+max = a if a > b else b
+print(max)  # 20
+
+# a가 짝수면 "짝" , 홀수면 "홀" 출력
+print("짝") if a % 2 == 0 else print("홀")
+
+
+# 90점 이상이면 A, 80점 이상이면 B, 70점 이상이면 C, 70점 미만이면 F
+score = 85
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# elif score >= 70:
+#     print("C")
+# else:
+#     print("F")
+
+grade = "A" if score >= 90 else "B" if score >= 80 else "C" if score >= 70 else "F"
+print(grade)
+
+
