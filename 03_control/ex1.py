cd835fdd7c70446499e7ef90e6d57c7eefabd44f
seojeonghoo <seojh0800@gmail.com>
2026-08-28 10:49:24 +0900

    fssdf

---
+++ 03_control/ex1.py
@@ -0,0 +1,23 @@
+score = 85 
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("D")
+
+#match문
+grade = "A"
+
+match grade:
+    case "A":
+        print("우수")
+    case "B":
+        print("양호")
+    case "C":
+        print("보통")
+    case _: #default
+        print("알 수 없음")
