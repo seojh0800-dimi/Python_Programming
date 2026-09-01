61dc0420b7c2bb740d44a7e82fe5933cf80d9fea
seojeonghoo <seojh0800@gmail.com>
2026-09-01 14:24:21 +0900

    dwdw

--- 03_control/ex3.py
+++ 03_control/ex3.py
@@ -7,3 +7,23 @@
 
 for i in range(5):    # 0 ~ 4
     print(i, end=" ")
+
+
+a = range(5)    # 0 ~ 4
+print(a.start, a.stop, a.step)
+
+# 1~5
+for i in range(1, 6):
+    print(i, end=" ")
+print()
+
+# 1~10, 2칸씩 뛰기
+for i in range(1, 11, 2):
+    print(i, end=" ")
+print()
+
+
+# 5 4 3 2 1 거꾸로
+
+for i in range(5, 0, -1):
+    print(i, end=" ")
