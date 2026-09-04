731ed5a6a93c0efe3cbb01998004569b205fc4f9
seojeonghoo <seojh0800@gmail.com>
2026-09-04 10:44:32 +0900

    Auto-commit: 2026-09-04 10:44:32

--- 04_collection/ex2.py
+++ 04_collection/ex2.py
@@ -98,7 +98,8 @@ subjects = ["국어", "수학", "영어"]
 scores = [80, 90, 95]
 
 zip(subjects, scores)
-print(a,b,c)
+a, b, c = zip(subjects, scores)
+print(a, b, c)
 print(list(zip(subjects, scores)))
 
 # ===========================================================
