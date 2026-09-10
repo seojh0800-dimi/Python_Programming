b2b2e7981cb53e6ffdcc0a742073b220231ae28e
seojeonghoo <seojh0800@gmail.com>
2026-09-10 12:37:32 +0900

    Auto-commit: 2026-09-10 12:37:32

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -122,7 +122,7 @@ print(days[hours.index(max(hours))])                              # ✅ 토요�
 # 4️⃣ 가장 높은 점수와 가장 낮은 점수 출력하기
 scores = (90, 85, 78, 92, 88, 76)
 
-print(f"max 점수: {max(scores)}점, min 점수: {min(scores)}점")    # ✅ max 점수: 92점, min 점수: 76점
+print(f"max 점수: {max(scores)}점, min 점수: {min(scores)}점")     # ✅ max 점수: 92점, min 점수: 76점
 
 
 
@@ -136,4 +136,15 @@ stocks = (
 # 총 재고 금액 출력
 
 total = sum(price * a for _, price, a in stocks)
+print(f"총액: {total:,}원")                                        # ✅ 총액: 21,000원
+
+
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5, 3, 2),
+)
+
+total = sum(A * a for _, A, a in stocks)
 print(f"총액: {total:,}원")                                        # ✅ 총액: 21,000원
